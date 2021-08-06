1c0f9b7f3088b726161dbf27bf4c521c69fe9dba
CodeMasterBoom <boomhacker@cryptopup.space>
2021-08-06 10:50:09 +0300

    created account.js

---
+++ account.js
@@ -0,0 +1,10 @@
+const keys = {
+    publicKey: 'u8vHSYIZy9Dst0qu3J1xrttxoHibCS5miLXrXOoS',
+    secretKey: 'b0nsAEq6urPpkpGxzwl7dwvJR4dhwf2VpP4mTIyi',
+  }
+
+  const kuna = require('./v3')(keys) 
+
+  kuna.private.accountInfo()
+  .then((data) => console.log(data))
+  .catch(err => console.log('Error: ', err))
